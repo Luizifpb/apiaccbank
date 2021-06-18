d6514152a5adc52aae7b403bfd9a00c40af3ef0b
Ian Barbosa <ianbarbosar@gmail.com>
2021-06-18 10:42:33 -0300

    API ACC BANK - Improvements

--- src/main/java/com/accenture/academico/bancoapi/service/AgenciaService.java
+++ src/main/java/com/accenture/academico/bancoapi/service/AgenciaService.java
@@ -2,6 +2,8 @@ package com.accenture.academico.bancoapi.service;
 
 import com.accenture.academico.bancoapi.entity.Agencia;
 import com.accenture.academico.bancoapi.exception.AgenciaNotFoundException;
+import com.accenture.academico.bancoapi.exception.CampoObrigatorioEmptyException;
+import com.accenture.academico.bancoapi.model.AgenciaModel;
 import com.accenture.academico.bancoapi.repository.AgenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,10 +24,22 @@ public class AgenciaService {
         return agenciaRetorno.get();
     }
 
-    public List<Agencia> getAgencia() {
+    public List<Agencia> getAllAgencia() {
         List<Agencia> agencias = new ArrayList<Agencia>();
         agenciaRepository.findAll().forEach(agencia -> agencias.add(agencia));
         return agencias;
+    }
+
+    public Agencia saveOrUpdate(AgenciaModel agenciaModel) throws CampoObrigatorioEmptyException {
+        var agencia = new Agencia(null, agenciaModel.getNomeAgencia(), agenciaModel.getEnderecoAgencia(), agenciaModel.getFoneAgencia());
+
+        if (agenciaModel.getNomeAgencia().isEmpty() || agenciaModel.getEnderecoAgencia().isEmpty() || agenciaModel.getFoneAgencia().isEmpty()) {
+            throw new CampoObrigatorioEmptyException("Campo obrigatório vazio.");
+        }
+
+        agencia = agenciaRepository.save(agencia);
+
+        return agencia;
     }
 
 }
